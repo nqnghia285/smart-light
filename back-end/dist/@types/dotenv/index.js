1801767fd322eb2fba6779bb252bb547edf28bc2
nghia <nqnghia285@gmail.com>
2021-04-06 09:37:20 +0700

    new update

--- back-end/dist/@types/dotenv/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dotenvConfig = void 0;
-function dotenvConfig() {
-    if (process.env.NODE_ENV !== "production") {
-        require("dotenv").config();
-    }
-}
-exports.dotenvConfig = dotenvConfig;
