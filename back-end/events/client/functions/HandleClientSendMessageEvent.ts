41fcf9496a1d4f865fb6ae1577f8fcda41b3c8fe
nghia <nqnghia285@gmail.com>
2021-04-10 20:17:37 +0700

    new update

--- back-end/events/client/functions/HandleClientSendMessageEvent.ts
+++ back-end/events/client/functions/HandleClientSendMessageEvent.ts
@@ -2,7 +2,7 @@ import { authenticateUserFromReq } from "authenticate-user";
 import { Socket } from "socket.io";
 import { ClientEvent, RequestType } from "../../../interface";
 
-export function handleClientSendMessageEvent(socket: Socket) {
+function handleClientSendMessageEvent(socket: Socket) {
     socket.on(ClientEvent.CLIENT_SEND_MESSAGE, async (message) => {
         const req: RequestType = socket.request;
         console.log("Cookies of client:", req.cookies);
